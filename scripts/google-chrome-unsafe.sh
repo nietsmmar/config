ca42abf8959894288dd62029df7cab196a282036
Raphael Fritz <raphael.fritz@articlett.de>
2024-03-23 13:06:18 +0100

    update config

---
+++ scripts/google-chrome-unsafe.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/google-chrome-stable --disable-web-security --user-data-dir="/home/raphael/development/ChromeTemp" $*
--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,4 @@
 ln -s $(realpath i3/config) $(realpath ~/.i3/config)
 ln -s $(realpath bash/bashrc) $(realpath ~/.bashrc)
 ln -s $(realpath kitty/kitty.conf) $(realpath ~/.config/kitty/kitty.conf)
+ln -s $(realpath i3/picom_config) $(realpath ~/.i3/picom_config)
