afc2a8644e7d04ea53a4fe702d447e02f6dfc6b8
Raphael Fritz <raphael.fritz@articlett.de>
2025-11-29 17:38:06 +0100

    update symlinks file

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,33 @@
-ln -s $(realpath i3/config) $(realpath ~/.i3/config)
-ln -s $(realpath bash/bashrc) $(realpath ~/.bashrc)
-ln -s $(realpath kitty/kitty.conf) $(realpath ~/.config/kitty/kitty.conf)
-ln -s $(realpath i3/picom_config) $(realpath ~/.i3/picom_config)
-ln -s $(realpath git/config) $(realpath ~/.config/git/config)
-ln -s $(realpath code/settings.json) $(realpath ~/.config/Code/User/settings.json)
-ln -s $(realpath zsh/zshrc.sh) $(realpath ~/.zshrc)
-ln -s $(realpath theme/gtk-4.0/assets) $(realpath ~/.config/gtk-4.0/assets)
-ln -s $(realpath theme/gtk-4.0/gtk.css) $(realpath ~/.config/gtk-4.0/gtk.css)
-ln -s $(realpath theme/gtk-4.0/gtk-dark.css) $(realpath ~/.config/gtk-4.0/gtk-dark.css)
-ln -s $(realpath theme/gtk-3.0/assets) $(realpath ~/.config/gtk-3.0/assets)
-ln -s $(realpath theme/gtk-3.0/gtk.css) $(realpath ~/.config/gtk-3.0/gtk.css)
-ln -s $(realpath theme/gtk-3.0/gtk-dark.css) $(realpath ~/.config/gtk-3.0/gtk-dark.css)
+CONFIG=$HOME/dev/config
+
+function make_symlink {
+    target="$CONFIG/$1"
+    name="$HOME/$2"
+    mkdir -p $(dirname "$name")
+    if [[ -L "$name" ]]; then
+        if [[ $(readlink -f "$name") != $target ]]; then
+            echo "Deleting previously existing symlink: $target"
+            rm $name
+            ln -s "$target" "$name"
+        fi
+    elif [[ ! -a "$name" ]]; then
+        echo "$target\t\t -> $name"
+        ln -s "$target" "$name"
+    else
+        echo "File already exists: $name"
+    fi
+}
+
+make_symlink i3/config .i3/config
+make_symlink bash/bashrc .bashrc
+make_symlink kitty/kitty.conf .config/kitty/kitty.conf
+make_symlink i3/picom_config .i3/picom_config
+make_symlink git/config .config/git/config
+make_symlink code/settings.json .config/Code/User/settings.json
+make_symlink zsh/zshrc.sh .zshrc
+make_symlink theme/gtk-4.0/assets .config/gtk-4.0/assets
+make_symlink theme/gtk-4.0/gtk.css .config/gtk-4.0/gtk.css
+make_symlink theme/gtk-4.0/gtk-dark.css .config/gtk-4.0/gtk-dark.css
+make_symlink theme/gtk-3.0/assets .config/gtk-3.0/assets
+make_symlink theme/gtk-3.0/gtk.css .config/gtk-3.0/gtk.css
+make_symlink theme/gtk-3.0/gtk-dark.css .config/gtk-3.0/gtk-dark.css
